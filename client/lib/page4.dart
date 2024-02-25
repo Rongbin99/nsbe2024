a3581c22383bff6619f7c4f6271f4e25f89f6855
Rongbin99 <rongbingu999@gmail.com>
2024-02-25 00:13:18 -0500

    the login

--- client/lib/page4.dart
+++ client/lib/page4.dart
@@ -1,27 +1,63 @@
 import 'package:flutter/material.dart';
+import 'login.dart';
+import 'signup.dart';
 
 class Page4 extends StatefulWidget {
-  const Page4({super.key});
+  const Page4({Key? key}) : super(key: key);
 
   @override
   _Page4State createState() => _Page4State();
 }
 
-class _Page4State extends State<Page4> {
+class _Page4State extends State<Page4> with SingleTickerProviderStateMixin {
+  late TabController? _tabController;
+
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+    _tabController?.addListener(_handleTabSelection);
+  }
+
+  void _handleTabSelection() {
+    if (_tabController!.indexIsChanging) {
+      switch (_tabController?.index) {
+        case 0:
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));
+          break;
+        case 1:
+          Navigator.push(context, MaterialPageRoute(builder: (context) => const SignupPage()));
+          break;
+      }
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Login & Signup'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Login'),
+            Tab(text: 'Signup'),
+          ],
+        ),
       ),
-      body: const Center(
-        child: Text('yerrr')
-      )
+      body: TabBarView(
+        controller: _tabController,
+        children: const [
+          LoginPage(),
+          SignupPage(),
+        ],
+      ),
     );
+  }
+
+  @override
+  void dispose() {
+    _tabController?.dispose();
+    super.dispose();
   }
 }
