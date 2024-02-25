3e3ef80e10296ee8f6bad4b7e6ab22d699d04008
Rongbin99 <rongbingu999@gmail.com>
2024-02-25 04:23:54 -0500

    font color changed

--- client/lib/login.dart
+++ client/lib/login.dart
@@ -88,7 +88,7 @@ class _LoginPageState extends State<LoginPage> {
           ),
           child: const Text(
             "Login",
-            style: TextStyle(fontSize: 20),
+            style: TextStyle(fontSize: 20, color: Colors.white),
           ),
         )
       ],
