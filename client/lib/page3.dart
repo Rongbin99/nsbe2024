1c62e5de845f1a2ccc5e633f08a00bf761c73b86
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2024-02-24 20:28:14 -0500

    camera added

--- client/lib/page3.dart
+++ client/lib/page3.dart
@@ -1,10 +1,102 @@
+// page3.dart
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'dart:io';
 
 class Page3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Camera',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Camera'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  CameraController? controller;
+  String imagePath = "";
+  late List<CameraDescription> cameras;
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera();
+  }
+
+  Future<void> initCamera() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    cameras = await availableCameras();
+    controller = CameraController(cameras[0], ResolutionPreset.max);
+    controller?.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (controller == null || !controller!.value.isInitialized) {
+      return Container();
+    }
     return Scaffold(
-      
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Container(
+                width: 200,
+                height: 200,
+                child: AspectRatio(
+                  aspectRatio: controller!.value.aspectRatio,
+                  child: CameraPreview(controller!),
+                ),
+              ),
+              TextButton(
+                  onPressed: () async {
+                    try {
+                      final image = await controller!.takePicture();
+                      setState(() {
+                        imagePath = image.path;
+                      });
+                    } catch (e) {
+                      print(e);
+                    }
+                  },
+                  child: Text("Take Photo")),
+              if (imagePath != "")
+                Container(
+                    width: 300,
+                    height: 300,
+                    child: Image.file(
+                      File(imagePath),
+                    ))
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
