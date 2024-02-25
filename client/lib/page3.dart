0fe74dac85715adafc42a2b20a8376866b541714
Luthira A <79228258+Luthiraa@users.noreply.github.com>
2024-02-25 08:39:30 -0500

    s

--- client/lib/page3.dart
+++ client/lib/page3.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-import 'package:gallery_saver/gallery_saver.dart';
 import 'camera.dart';
 import 'package:http/http.dart' as http;
 
